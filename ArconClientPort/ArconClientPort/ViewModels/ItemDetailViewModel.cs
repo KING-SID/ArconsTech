a4391d787c3ebc8636e5de2359c26341b57f9edf
Sidhant Bendre <sidhantbendre22@gmail.com>
2019-03-24 17:49:09 -0400

    Add project files.

---
+++ ArconClientPort/ArconClientPort/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ArconClientPort.Models;
+
+namespace ArconClientPort.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
